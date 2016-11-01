f04e246b03cbeb5e56ce7d26100272458a7ec3b6
Inaguma Hirofumi <hirofumi@h79.49.229.10.1016757.vlan.kuins.net>
2016-11-01 18:15:05 +0900

    add some comments

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 # Purpose
 多人数会議における議事録の自動生成
+
+# Referrence
+[SkyWay-MultiParty](https://github.com/nttcom/SkyWay-MultiParty)  
+[↑の記事](https://html5experts.jp/sakkuru/16397/)
--- src/js/room.js
+++ src/js/room.js
@@ -1,122 +1,134 @@
 
     var multiparty;
-    var APIKEY = '3d27e703-8d24-441b-9b22-773bd1c9a181';
 
+    // SkyWayサーバに接続し，peerに接続
     function start() {
-      if (localStorage.chatLog === undefined) { localStorage.chatLog = JSON.stringify([]); }
+        if (localStorage.chatLog === undefined) { localStorage.chatLog = JSON.stringify([]); }
 
-      // MultiParty インスタンスを生成
-      multiparty = new MultiParty( {
-        "key": APIKEY,
-        "reliable": true,
-        "debug": 3
-      });
+        // MultiPartyインスタンスを生成
+        multiparty = new MultiParty( {
+            "key": '3d27e703-8d24-441b-9b22-773bd1c9a181',  // APIキー
+            "reliable": true,  // データチャンネルで信頼性のあるデータ転送(sctp)を行う
+            "debug": 3  // すべてのログを表示
+        });
 
-      /////////////////////////////////
-      // for MediaStream
-      multiparty.on('my_ms', function(video) {
-        // 自分のvideoを表示
-        var vNode = MultiParty.util.createVideoNode(video);
-        vNode.setAttribute("class", "video my-video");
-        vNode.volume = 0;
-        $(vNode).appendTo("#streams");
-      }).on('peer_ms', function(video) {
-        // console.log("video received!!");
-        // peerのvideoを表示
-        // console.log(video);
-        var vNode = MultiParty.util.createVideoNode(video);
-        vNode.setAttribute("class", "video peer-video");
-        $(vNode).appendTo("#streams");
-        // console.log($("#streams"));
-      }).on('ms_close', function(peer_id) {
-        // peerが切れたら、対象のvideoノードを削除する
-        $("#"+peer_id).remove();
-      }).on('open', function(id){
-        peer_id = id;
-        console.log(id);
-      });
 
-      ////////////////////////////////
-      // for DataChannel
-      multiparty.on('message', function(mesg) {
-        // peerからテキストメッセージを受信
-        $("p.receive").append(mesg.data + "<br>");
-      });
+        /////////////////////////////////
+        // for MediaStream
+        /////////////////////////////////
+        // このウィンドウのvideo/audioストリームのセットアップ完了時
+        multiparty.on('my_ms', function(video) {
+            // 自分のvideoを表示
+            var vNode = MultiParty.util.createVideoNode(video);
+            vNode.setAttribute("class", "video my-video");
+            vNode.volume = 0;
+            $(vNode).appendTo("#streams");
 
-      ////////////////////////////////
-      // エラーハンドラ
-      multiparty.on('error', function(err) {
-        alert(err);
-      });
+        // peerのvideo/audioストリームのセットアップ完了時
+        }).on('peer_ms', function(video) {
+            // peerのvideoを表示
+            var vNode = MultiParty.util.createVideoNode(video);
+            vNode.setAttribute("class", "video peer-video");
+            $(vNode).appendTo("#streams");
 
-      multiparty.start();
+        // peerのメディアストリームのクローズ時
+        }).on('ms_close', function(peer_id) {
+            // peerが切れたら、対象のvideoノードを削除する
+            $("#"+peer_id).remove();
 
-      //////////////////////////////////////////////////////////
-      // テキストフォームに入力されたテキストをpeerに送信
-      $("#message form").on("submit", function(ev) {
-        ev.preventDefault();  // onsubmitのデフォルト動作（reload）を抑制
+        // SkyWayサーバとのコネクション確立時
+        }).on('open', function(id){
+            peer_id = id;
+            // console.log(id);
+        });
 
-        // テキストデータ取得
-        var $text = $(this).find("input[type=text]");
-        var data = $text.val();
-        // 時間取得
-        var time = parseInt(Date.now() / 1000, 10);
-        // オブジェクトに変換
-        var temp = JSON.parse(localStorage.chatLog);
-        console.log(temp);
-        // push
-        if (data != "") {
-          obj = {};
-          obj[time] = {'text': data, 'id': 1};
-          temp.push(obj);
-        }
-        // 文字列に変換
-        localStorage.chatLog = JSON.stringify(temp);
-        console.log(localStorage.chatLog);
 
-        if(data.length > 0) {
-          data = data.replace(/</g, "&lt;").replace(/>/g, "&gt;");
-          $("p.receive").append(data + "<br>");
+        ////////////////////////////////
+        // for DataChannel
+        /////////////////////////////////
+        // peerから受信したメッセージを表示
+        multiparty.on('message', function(mesg) {
+            $("p.receive").append(mesg.data + "<br>");
+        });
 
-          // メッセージを接続中のpeerに送信する
-          multiparty.send(data);
-          $text.val("");
-        }
-      });
+        // 接続中のpeerにメッセージを送信
+        $("#message form").on("submit", function(ev) {
+            ev.preventDefault();  // onsubmitのデフォルト動作（reload）を抑制
 
-      ///////////////////////////////////////////////////
-      // handle mute/unmute
-      $("#video-mute").on("click", function(ev) {
-        var mute = !$(this).data("muted");
-        multiparty.mute({video: mute});
-        $(this).text("video " + (mute ? "unmute" : "mute")).data("muted", mute);
-      });
+            // テキストデータ取得
+            var $text = $(this).find("input[type=text]");
+            var data = $text.val();
+            // 時間取得
+            var time = parseInt(Date.now() / 1000, 10);
+            // オブジェクトに変換
+            var temp = JSON.parse(localStorage.chatLog);
+            console.log(temp);
+            // push
+            if (data != "") {
+              obj = {};
+              obj[time] = {'text': data, 'id': 1};
+              temp.push(obj);
+            }
+            // 文字列に変換
+            localStorage.chatLog = JSON.stringify(temp);
+            console.log(localStorage.chatLog);
 
-      $("#audio-mute").on("click", function(ev) {
-        var mute = !$(this).data("muted");
-        multiparty.mute({audio: mute});
-        $(this).text("audio " + (mute ? "unmute" : "mute")).data("muted", mute);
-      });
+            if(data.length > 0) {
+                data = data.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+                $("p.receive").append(data + "<br>");
 
-      // 通話終了のイベント
-      $("#finish").on("click", function(ev) {
-        var result = {};
-        // オブジェクトに変換
-        chatLog = JSON.parse(localStorage.chatLog);
-        voiceLog = JSON.parse(localStorage.voiceLog);
-        var result = {};
-        for (var i in chatLog) {
-          console.log(chatLog[i]);
-          result.push(chatLog[i]);
-        }
-        for (var i in voiceLog) {
-          result.push(voiceLog[i]);
-        }
-        console.log(result);
-      });
+                // メッセージを接続中のpeerに送信
+                multiparty.send(data);
+                $text.val("");
+            }
+        });
+
+
+        ////////////////////////////////
+        // Error handling
+        /////////////////////////////////
+        multiparty.on('error', function(err) {
+            alert(err);
+        });
+
+        multiparty.start();
+
+
+        /////////////////////////////////
+        // handle mute/unmute
+        /////////////////////////////////
+        // ビデオのミュート
+        $("#video-mute").on("click", function(ev) {
+            var mute = !$(this).data("muted");
+            multiparty.mute({video: mute});
+            $(this).text("video " + (mute ? "unmute" : "mute")).data("muted", mute);
+        });
+        // 音声のミュート
+        $("#audio-mute").on("click", function(ev) {
+            var mute = !$(this).data("muted");
+            multiparty.mute({audio: mute});
+            $(this).text("audio " + (mute ? "unmute" : "mute")).data("muted", mute);
+        });
+
+        /////////////////////////////////
+        // 通話終了のイベント
+        /////////////////////////////////
+        $("#finish").on("click", function(ev) {
+            var result = {};
+            // オブジェクトに変換
+            chatLog = JSON.parse(localStorage.chatLog);
+            voiceLog = JSON.parse(localStorage.voiceLog);
+            var result = {};
+            for (var i in chatLog) {
+              console.log(chatLog[i]);
+              result.push(chatLog[i]);
+            }
+            for (var i in voiceLog) {
+              result.push(voiceLog[i]);
+            }
+            console.log(result);
+        });
     }
-
-
 
 $(document).ready(function(){
     start();
